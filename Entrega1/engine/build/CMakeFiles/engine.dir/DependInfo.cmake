7ec6d0d778c913da71bdbc4ff30ad3c94b1b61b0
goncalo <gduarterodrigues321@gmail.com>
2023-10-02 16:59:31 +0100

    final project CG

---
+++ Entrega1/engine/build/CMakeFiles/engine.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/goncalo/Desktop/tpCG/Entrega1/engine/engine.cpp" "CMakeFiles/engine.dir/engine.cpp.o" "gcc" "CMakeFiles/engine.dir/engine.cpp.o.d"
+  "/home/goncalo/Desktop/tpCG/Entrega1/engine/tinyxml/tinyxml2.cpp" "CMakeFiles/engine.dir/tinyxml/tinyxml2.cpp.o" "gcc" "CMakeFiles/engine.dir/tinyxml/tinyxml2.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
